1a8d51adf4aa506178676287ce6585fcc94a0285
root <admin@ldc.int>
2018-10-01 20:27:05 +0800

    add ssh-agent for ansible playbooks

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Execute terraform
 cd /home/master/Projects/terravSphere
 terraform apply -var-file=~/Projects/terravSphere/Swarm/swarm.tfvars -var-file=~/Projects/terravSphere/DR.tfvars -state=~/Projects/terravSphere/Swarm/swarm.tfstate
@@ -6,5 +7,9 @@ terraform apply -var-file=~/Projects/terravSphere/Swarm/swarm.tfvars -var-file=~
 python ~/Projects/executor/buildAnsibleInventory.py "/home/master/Projects/terravSphere/Swarm/swarm.tfvars"
 
 # finalize build with ansible-playbook
+# setup ssh agent for private key
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+# start ansible
 cd ~/Projects/host-Config/
 ansible-playbook -i ~/Projects/host-Config/inventory/inventory-swarm --private-key=/home/master/.ssh/id_rsa terra-final.yml
